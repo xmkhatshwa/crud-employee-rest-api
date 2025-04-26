56e8e08463253d60e92afcb7dbedc9156456cca8
xmkhatshwa <mkhatshwa.xola@gmail.com>
2025-04-26 05:00:55 +0200

    Initial commit

---
+++ src/main/java/com/xolamkhatshwa/crud_employee_rest_api/CrudEmployeeRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.xolamkhatshwa.crud_employee_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEmployeeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEmployeeRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xolamkhatshwa/crud_employee_rest_api/CrudEmployeeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xolamkhatshwa.crud_employee_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudEmployeeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
